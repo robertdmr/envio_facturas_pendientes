120fdf62c777a29453e93251f364a2768b8329bd
RobertM <robertomosqueda@outlook.com>
2026-09-02 14:14:34 -0300

    test: reset efactura config baseline for deterministic tests

--- tests/Feature/ConfiguracionTest.php
+++ tests/Feature/ConfiguracionTest.php
@@ -12,7 +12,13 @@ class ConfiguracionTest extends TestCase
     {
         parent::setUp();
         DB::connection()->beginTransaction();
-        ParametroEfactura::registroUnico();
+        ParametroEfactura::registroUnico()->update([
+            'contribuyente_id' => 33,
+            'pass' => 'c6e8a6f0d815e1fd9358d76b208799efbb40c6d8155be29903dcbe5b99125c07',
+            'timbrado' => '12558948',
+            'fec_inicio' => '2021-08-25 00:00:00',
+            'sucursal' => 'Central',
+        ]);
     }
 
     protected function tearDown(): void
